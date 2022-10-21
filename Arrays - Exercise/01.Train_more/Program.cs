8165e89ca59c6ac297f5879fa249ee80e539a164
enginyashar <114264911+enginyashar@users.noreply.github.com>
2022-10-21 19:51:12 +0300

    Add files via upload

---
+++ Arrays - Exercise/01.Train_more/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+namespace _01.Train_more
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] people = new int[n];
+
+            for (int i = 0; i < people.Length; i++)
+            {
+                people[i] = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine(string.Join(" ", people));
+            Console.WriteLine(people.Sum());
+        }
+    }
+}
