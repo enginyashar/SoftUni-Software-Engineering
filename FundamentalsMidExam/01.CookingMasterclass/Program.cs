89a7b8dbc43f47bf86d1e7612df4bc2ef37a4019
enginyashar <114264911+enginyashar@users.noreply.github.com>
2022-10-23 23:02:47 +0300

    Add files via upload

---
+++ FundamentalsMidExam/01.CookingMasterclass/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _01.CookingMasterclass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double budget = double.Parse(Console.ReadLine());
+            int students = int.Parse(Console.ReadLine());
+            double flourPrice = double.Parse(Console.ReadLine());
+            double eggPrice = double.Parse(Console.ReadLine());
+            double apronPrice = double.Parse(Console.ReadLine());
+
+            Console.WriteLine(Math.Floor(1.0 * students / 5));
+            double total = (flourPrice * (students - Math.Floor(students / 5.0))) + (10 * eggPrice * students) + (students * Math.Ceiling(1.2 * apronPrice));
+
+            if (total <= budget)
+            {
+                Console.WriteLine($"Items purchased for {total:f2}$");
+            }
+            else
+            {
+                Console.WriteLine($"{total - budget:f2}$ more needed.");
+            }
+        }
+    }
+}
---
+++ FundamentalsMidExam/01.TheHuntingGames/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace _01.TheHuntingGames
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int player = int.Parse(Console.ReadLine());
+            double energy = double.Parse(Console.ReadLine());
+            double water = double.Parse(Console.ReadLine()) * player * n;
+            double food = double.Parse(Console.ReadLine()) * player * n;
+            bool isThereEnergy = true;
+
+            for (int i = 1; i <= n; i++)
+            {
+                double energyLoss = double.Parse(Console.ReadLine());
+                energy -= energyLoss;
+
+                if (energy <= 0)
+                {
+                    isThereEnergy = false;
+                    Console.WriteLine($"You will run out of energy. You will be left with {food:f2} food and {water:f2} water.");
+                    break;
+                }
+
+                if (i % 2 == 0 && water >= 0)
+                {
+                    water -= (water * 0.3);
+                    energy *= 1.05;
+                }
+
+                if (i % 3 == 0 && food >= 0)
+                {
+                    food -= (food / player);
+                    energy *= 1.1;
+                }                
+            }
+            if (isThereEnergy)
+            {
+                Console.WriteLine($"You are ready for the quest. You will be left with - {energy:f2} energy!");
+            }
+        }
+    }
+}
