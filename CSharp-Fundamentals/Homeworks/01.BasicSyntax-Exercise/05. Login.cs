ef7b0dccb04261475d637135f362b5145814fb49
enginyashar <114264911+enginyashar@users.noreply.github.com>
2022-12-30 11:38:12 +0200

    Update 05. Login.cs

--- CSharp-Fundamentals/Homeworks/01.BasicSyntax-Exercise/05. Login.cs
+++ CSharp-Fundamentals/Homeworks/01.BasicSyntax-Exercise/05. Login.cs
@@ -1,1 +1,39 @@
 
+using System;
+
+namespace _05.Login
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var username = Console.ReadLine();
+            char[] charArray = username.ToCharArray();
+            Array.Reverse(charArray);
+            var password = new string(charArray);
+            var psw = string.Empty;
+
+            var failCnt = 0;
+
+            for (int i = 0; i <= 4; i++)
+            {
+                psw = Console.ReadLine();
+                if (psw == password)
+                {
+                    Console.WriteLine($"User {username} logged in.");
+                    break;
+                }
+                else
+                {
+                    failCnt++;
+                    if (failCnt == 4)
+                    {
+                        Console.WriteLine($"User {username} blocked!");
+                        break;
+                    }
+                    Console.WriteLine("Incorrect password. Try again.");
+                }
+            }                                
+        }
+    }
+}
