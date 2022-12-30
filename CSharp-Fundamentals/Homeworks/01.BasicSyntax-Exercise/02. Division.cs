f6fdc17d95aba7f88822f0a5d845d0a6ee059a5a
enginyashar <114264911+enginyashar@users.noreply.github.com>
2022-12-30 11:36:41 +0200

    Update 02. Division.cs

--- CSharp-Fundamentals/Homeworks/01.BasicSyntax-Exercise/02. Division.cs
+++ CSharp-Fundamentals/Homeworks/01.BasicSyntax-Exercise/02. Division.cs
@@ -1,1 +1,47 @@
+using System;
 
+namespace _02.Division
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var num = double.Parse(Console.ReadLine());
+            var max = 0;
+
+            if (num % 2 == 0)
+            {
+                max = 2;
+                if (num % 10 == 0)
+                {
+                    max = 10;
+                }
+                else if (num % 3 == 0 && num % 6 == 0)
+                {
+                    max = 6;
+                }
+            }
+            else if (num % 3 == 0)
+            {
+                max = 3;
+                if (num % 7 == 0)
+                {
+                    max = 7;
+                }
+            }
+            else if (num % 7 == 0)
+            {
+                max = 7;
+            }
+
+            if (max == 0)
+            {
+                Console.WriteLine("Not divisible");
+            }
+            else
+            {
+                Console.WriteLine($"The number is divisible by {max}");
+            }
+        }
+    }
+}
