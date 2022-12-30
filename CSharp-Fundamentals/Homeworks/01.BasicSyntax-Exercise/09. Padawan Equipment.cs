3fdf6aa9b2c9e0f6f77d3de57b6d3377f4dcc57a
enginyashar <114264911+enginyashar@users.noreply.github.com>
2022-12-30 11:41:22 +0200

    Update 09. Padawan Equipment.cs

--- CSharp-Fundamentals/Homeworks/01.BasicSyntax-Exercise/09. Padawan Equipment.cs
+++ CSharp-Fundamentals/Homeworks/01.BasicSyntax-Exercise/09. Padawan Equipment.cs
@@ -1,1 +1,36 @@
 
+using System;
+
+namespace _09.PadawanEquipment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var amount = double.Parse(Console.ReadLine());
+            var students = int.Parse(Console.ReadLine());
+            var lightsaberPrice = double.Parse(Console.ReadLine());
+            var robePrice = double.Parse(Console.ReadLine());
+            var beltPrice = double.Parse(Console.ReadLine());
+
+            int freeBelt = 0;
+
+            if (students >= 6)
+            {
+                freeBelt = students / 6;
+            }
+
+            var total = (Math.Ceiling(students * 1.1) * lightsaberPrice) + (students * robePrice) + ((students - freeBelt) * beltPrice);
+            
+            if (amount >= total)
+            {
+                Console.WriteLine($"The money is enough - it would cost {total:f2}lv.");
+            }
+            else
+            {
+                total -= amount;
+                Console.WriteLine($"John will need {total:f2}lv more.");
+            }
+        }
+    }
+}
