f576820a5a89346066198ccd27b2f4dc9e1a4026
enginyashar <114264911+enginyashar@users.noreply.github.com>
2022-12-30 11:42:47 +0200

    Update 01. Ages.cs

--- CSharp-Fundamentals/Homeworks/01.BasicSyntax-Exercise/01. Ages.cs
+++ CSharp-Fundamentals/Homeworks/01.BasicSyntax-Exercise/01. Ages.cs
@@ -1,1 +1,35 @@
+using System;
 
+namespace _01.Ages
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var age = int.Parse(Console.ReadLine());
+            var person = string.Empty;
+
+            if (age >= 66)
+            {
+                person = "elder";
+            }
+            else if (age >= 20)
+            {
+                person = "adult";
+            }
+            else if (age >= 14)
+            {
+                person = "teenager";
+            }
+            else if (age >= 3)
+            {
+                person = "child";
+            }
+            else
+            {
+                person = "baby";
+            }
+            Console.WriteLine(person);
+        }
+    }
+}
