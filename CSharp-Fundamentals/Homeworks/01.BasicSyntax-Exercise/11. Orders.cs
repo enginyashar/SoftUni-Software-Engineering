2a14882aa4577d97774e6091ed881835fccf5dd3
enginyashar <114264911+enginyashar@users.noreply.github.com>
2022-12-30 11:42:16 +0200

    Update 11. Orders.cs

--- CSharp-Fundamentals/Homeworks/01.BasicSyntax-Exercise/11. Orders.cs
+++ CSharp-Fundamentals/Homeworks/01.BasicSyntax-Exercise/11. Orders.cs
@@ -1,1 +1,26 @@
+using System;
 
+namespace _11.Orders
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            double price = 0.0;
+            double total = 0.0;
+
+            for (int i = 0; i < n; i++)
+            {
+                double pricePerCapsule = double.Parse(Console.ReadLine());
+                int daysInMonth = int.Parse(Console.ReadLine());
+                int capsulesCnt = int.Parse(Console.ReadLine());
+
+                price = ((daysInMonth * capsulesCnt) * pricePerCapsule);
+                Console.WriteLine($"The price for the coffee is: ${price:f2}");
+                total += price;
+            }
+            Console.WriteLine($"Total: ${total:f2}");
+        }
+    }
+}
