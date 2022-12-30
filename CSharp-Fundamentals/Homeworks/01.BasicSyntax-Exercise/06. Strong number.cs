c2e0a5ae0d8e65f97fe0cde6f1de791979c9e60a
enginyashar <114264911+enginyashar@users.noreply.github.com>
2022-12-30 11:38:54 +0200

    Update 06. Strong number.cs

--- CSharp-Fundamentals/Homeworks/01.BasicSyntax-Exercise/06. Strong number.cs
+++ CSharp-Fundamentals/Homeworks/01.BasicSyntax-Exercise/06. Strong number.cs
@@ -1,1 +1,43 @@
+using System;
+
+namespace _06.StrongNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var num = int.Parse(Console.ReadLine());
+            var number = num;
+            var currentNum = 0;
+            var factorialSum = 0;
+
+            while (number != 0)
+            {
+                var factorial = 1;
+
+                currentNum = number % 10;
+                number /= 10;
+
+                for (int i = 1; i <= currentNum; i++)
+                {
+                    factorial *= i;
+                }
+
+                factorialSum += factorial;
+            }
+            var result = string.Empty;
+
+            if (num == factorialSum)
+            {
+                result = "yes";
+            }
+            else
+            {
+                result = "no";
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}
 
