529fb2410cdc7be1b74e584160d88435ad4d09cb
enginyashar <114264911+enginyashar@users.noreply.github.com>
2022-12-30 11:15:46 +0200

    Create 09. Sum of Odd Numbers.cs

---
+++ CSharp-Fundamentals/Homeworks/01.BasicSyntax-Lab/09. Sum of Odd Numbers.cs
@@ -0,0 +1,20 @@
+using System;
+
+namespace _09.SumOfOddNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+            var sum = 0;
+
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine(2 * i - 1);
+                sum += 2 * i - 1;                             
+            }
+            Console.WriteLine($"Sum: {sum}");
+        }
+    }
+}
