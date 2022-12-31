f88bfa9aa0d2f9ab2cc9b0a70dc86515e13214b6
enginyashar <114264911+enginyashar@users.noreply.github.com>
2022-12-31 11:56:26 +0200

    Create 12. Refactor Special Numbers.cs

---
+++ CSharp-Fundamentals/Homeworks/02.Data Types and Variables - Lab/12. Refactor Special Numbers.cs
@@ -0,0 +1,30 @@
+using System;
+
+namespace _12.RefactorSpecialNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int sum = 0;
+            int num = 0;
+            bool isSpecial = false;
+
+            for (int i = 1; i <= n; i++)
+            {
+                num = i;
+                while (i > 0)
+                {
+                    sum += i % 10;
+                    i = i / 10;
+                }
+
+                isSpecial = (sum == 5) || (sum == 7) || (sum == 11);
+                Console.WriteLine("{0} -> {1}", num, isSpecial);
+                sum = 0;
+                i = num;
+            }
+        }
+    }
+}
