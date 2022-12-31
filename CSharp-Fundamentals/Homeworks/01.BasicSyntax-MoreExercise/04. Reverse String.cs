cf6d4491c3c33477beb27aafcf6d2f50412f39f7
enginyashar <114264911+enginyashar@users.noreply.github.com>
2022-12-31 11:42:29 +0200

    Create 04. Reverse String.cs

---
+++ CSharp-Fundamentals/Homeworks/01.BasicSyntax-MoreExercise/04. Reverse String.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace _04.ReverseString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            string reverse = string.Empty;
+
+            for (int i = text.Length - 1; i >= 0 ; i--)
+            {
+                reverse += text[i];
+            }
+            Console.WriteLine(reverse);
+        }
+    }
+}
