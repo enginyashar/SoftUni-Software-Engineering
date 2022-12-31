adb01bf30ec1f74c6c2d6e7811ccf4f6217cdc50
enginyashar <114264911+enginyashar@users.noreply.github.com>
2022-12-31 13:17:27 +0200

    Create 06. Triples of Latin Letters.cs

---
+++ CSharp-Fundamentals/Homeworks/02. Data Types and Variables - Exercise/06. Triples of Latin Letters.cs
@@ -0,0 +1,23 @@
+using System;
+
+namespace _06._Triples_of_Latin_Letters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    for (int k = 0; k < n; k++)
+                    {
+                        Console.WriteLine($"{(char)('a' + i)}{(char)('a' + j)}{(char)('a' + k)}");
+                    }
+                }
+            }
+        }
+    }
+}
