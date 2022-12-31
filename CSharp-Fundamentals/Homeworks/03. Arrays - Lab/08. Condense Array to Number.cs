2cc342b839d9dae541ee832926abf6686d42dde8
enginyashar <114264911+enginyashar@users.noreply.github.com>
2022-12-31 22:38:13 +0200

    Create 08. Condense Array to Number.cs

---
+++ CSharp-Fundamentals/Homeworks/03. Arrays - Lab/08. Condense Array to Number.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Linq;
+
+namespace _08._Condense_Array_to_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            while (nums.Length != 1)
+            {
+                int[] condensed = new int[nums.Length - 1];
+
+                for (int i = 0; i < condensed.Length; i++)
+                {
+                    condensed[i] = nums[i] + nums[i + 1];
+                }
+
+                nums = condensed;
+            }
+
+            Console.WriteLine(string.Join("", nums));
+        }
+    }
+}
