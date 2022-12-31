7b7c69b9f7e185628953fc1b82d6f790a224b3ae
enginyashar <114264911+enginyashar@users.noreply.github.com>
2022-12-31 11:53:48 +0200

    Create 07. Concat Names.cs

---
+++ CSharp-Fundamentals/Homeworks/02.Data Types and Variables - Lab/07. Concat Names.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace _07.ConcatNames
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string ch1 = Console.ReadLine();
+            string ch2 = Console.ReadLine();
+            string ch3 = Console.ReadLine();
+          
+            Console.WriteLine(ch1 + ch3 + ch2);
+        }
+    }
+}
