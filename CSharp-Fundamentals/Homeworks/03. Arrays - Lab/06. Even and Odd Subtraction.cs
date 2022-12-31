521293a5fbdd21a6731821480bf4ab7eb79c92ad
enginyashar <114264911+enginyashar@users.noreply.github.com>
2022-12-31 22:37:37 +0200

    Create 06. Even and Odd Subtraction.cs

---
+++ CSharp-Fundamentals/Homeworks/03. Arrays - Lab/06. Even and Odd Subtraction.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Linq;
+
+namespace _06.EvenAndOddSubtraction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+            int sum = 0;
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] % 2 == 0)
+                {
+                    sum += numbers[i];
+                }
+                else
+                {
+                    sum -= numbers[i];
+                }
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
