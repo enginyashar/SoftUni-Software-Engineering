7e137b81cfc5975175c87744932abf8c14041a5c
enginyashar <114264911+enginyashar@users.noreply.github.com>
2023-01-02 22:33:02 +0200

    Create 02. Center Point.cs

---
+++ CSharp-Fundamentals/Homeworks/04. Methods - More Exercise/02. Center Point.cs
@@ -0,0 +1,31 @@
+using System;
+
+namespace _02.CenterPoint
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x1 = double.Parse(Console.ReadLine());
+            double y1 = double.Parse(Console.ReadLine());
+            double x2 = double.Parse(Console.ReadLine());
+            double y2 = double.Parse(Console.ReadLine());
+
+            double distance1 = GetDistance(x1, y1, 0, 0);
+            double distance2 = GetDistance(x2, y2, 0, 0);
+
+            if (distance1 > distance2)
+            {
+                Console.WriteLine($"({x2}, {y2})");
+            }
+            else
+            {
+                Console.WriteLine($"({x1}, {y1})");
+            }
+        }
+        static double GetDistance(double x1, double y1, double x2, double y2)
+        {
+            return Math.Sqrt(Math.Pow(x2 - x1, 2.0) + Math.Pow(y2 - y1, 2.0));
+        }
+    }
+}
