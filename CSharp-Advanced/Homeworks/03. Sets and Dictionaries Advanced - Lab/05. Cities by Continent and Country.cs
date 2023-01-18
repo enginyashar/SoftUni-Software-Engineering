d8729b710478266adbd9698e9d2d6bdbb701c059
enginyashar <114264911+enginyashar@users.noreply.github.com>
2023-01-18 23:13:23 +0200

    Create 05. Cities by Continent and Country.cs

---
+++ CSharp-Advanced/Homeworks/03. Sets and Dictionaries Advanced - Lab/05. Cities by Continent and Country.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Collections.Generic;
+
+namespace _05._Cities_by_Continent_and_Country
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, Dictionary<string, List<string>>> continents = new Dictionary<string, Dictionary<string, List<string>>>();
+
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] input = Console.ReadLine().Split();
+                string continent = input[0];
+                string country = input[1];
+                string city = input[2];
+
+                if (!continents.ContainsKey(continent))
+                {
+                    continents.Add(continent, new Dictionary<string, List<string>>());
+                }
+
+                if (!continents[continent].ContainsKey(country))
+                {
+                    continents[continent].Add(country, new List<string>());
+                }
+
+                continents[continent][country].Add(city);
+
+                //if (!continents[continent][country].Contains(city))
+                //{
+
+                //}
+            }
+
+            foreach (var (continent, countries) in continents)
+            {
+                Console.WriteLine(continent + ":");
+
+                foreach (var (country, cities) in countries)
+                {
+                    Console.WriteLine($"  {country} -> {string.Join(", ", cities)}");
+                }
+            }
+        }
+    }
+}
