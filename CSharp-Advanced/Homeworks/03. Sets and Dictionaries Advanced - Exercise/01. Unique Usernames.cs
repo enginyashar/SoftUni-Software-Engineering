21ccbeadfd0478db1d6b264f7c33dc94c96fce94
enginyashar <114264911+enginyashar@users.noreply.github.com>
2023-01-21 11:48:00 +0200

    Create 01. Unique Usernames.cs

---
+++ CSharp-Advanced/Homeworks/03. Sets and Dictionaries Advanced - Exercise/01. Unique Usernames.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+
+namespace _01._Unique_Usernames
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> set = new HashSet<string>();
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                set.Add(Console.ReadLine());
+            }            
+
+            foreach (var name in set)
+            {
+                Console.WriteLine(name);
+            }
+        }
+    }
+}
