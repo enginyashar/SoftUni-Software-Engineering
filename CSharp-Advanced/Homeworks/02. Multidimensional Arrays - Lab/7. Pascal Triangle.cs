e0dcb8f12390d57007baeffa3a68da698baf71ed
enginyashar <114264911+enginyashar@users.noreply.github.com>
2023-01-22 10:22:48 +0200

    Create 7. Pascal Triangle.cs

---
+++ CSharp-Advanced/Homeworks/02. Multidimensional Arrays - Lab/7. Pascal Triangle.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Linq;
+
+namespace _7._Pascal_Triangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            long[][] pascal = new long[n][];
+            pascal[0] = new long[1] { 1 };
+
+            for (int row = 1; row < n; row++)
+            {
+                pascal[row] = new long[row + 1];
+
+                for (int col = 0; col < pascal[row].Length; col++)
+                {
+                    if (pascal[row - 1].Length > col)
+                    {
+                        pascal[row][col] += pascal[row - 1][col];
+                    }
+                    if (col > 0)
+                    {
+                        pascal[row][col] += pascal[row - 1][col - 1];
+                    }                    
+                }
+            }
+
+            foreach (var arr in pascal)
+            {
+                Console.WriteLine(string.Join(" ", arr));
+            }
+        }
+    }
+}
