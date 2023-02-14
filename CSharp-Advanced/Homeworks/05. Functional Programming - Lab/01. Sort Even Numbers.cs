027625ebb520023d05d22ebb1568f583341e771f
enginyashar <114264911+enginyashar@users.noreply.github.com>
2023-02-14 22:02:41 +0200

    Create 01. Sort Even Numbers.cs

---
+++ CSharp-Advanced/Homeworks/05. Functional Programming - Lab/01. Sort Even Numbers.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Linq;
+
+namespace _01._Sort_Even_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .Where(x => x % 2 == 0)
+                .OrderBy(x => x)
+                .ToArray();
+
+            Console.WriteLine(string.Join(", ", nums));
+        }
+    }
+}
